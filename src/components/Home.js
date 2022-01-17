704dec56874d9436a47572f42d502278e2d3b73e
alkanoidev <alkanoidev@gmail.com>
2022-01-17 13:23:25 +0100

    dasd

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 export default function Home() {
     return (
         <div className='home'>
-            <Link className='home-link' to="/nasaphoto">See into the stars!</Link>
+            <h1>Welcome to Nasa Apis Space Station!</h1>
+            <Link className='home-link' to="/nasaphoto">nasa picture of day!</Link>
         </div>
     )
 }
